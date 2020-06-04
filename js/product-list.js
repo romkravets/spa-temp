c71d17bd9a4ebbe49e1463f448a3fa3582ae452b
romkravets <romann.kravets@gmail.com>
2020-06-04 16:13:16 +0300

    base style product

--- js/product-list.js
+++ js/product-list.js
@@ -43,7 +43,7 @@ class ProductList {
                         <div class="card-price flex-fill d-flex flex-wrap">
                           <h5 class="${saleClass}">${product.price} грн</h5> ${priceSale}
                         </div>
-                          <button class="btn-main btn-accent buy" data-toggle="modal" data-target="#modal-cart"  data-id="${product.id}" alt="Laboratoties INELDEA. Професійні засоби по догляду за тілом та обличчям MEDICAFARM, EFFIDERM, Франція" title="Замовити">Замовити</button>
+                          <button class="btn-main btn-accent btn-product buy" data-toggle="modal" data-target="#modal-cart"  data-id="${product.id}" alt="Laboratoties INELDEA. Професійні засоби по догляду за тілом та обличчям MEDICAFARM, EFFIDERM, Франція" title="Замовити">Замовити</button>
                         </div>
                         <div class="title-width">
                           <h4 class="card-title">${product.title}</h4>
