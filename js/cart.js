22c3882acc9932e5d2d5317b6e615749f3323349
romkravets <romann.kravets@gmail.com>
2020-06-06 13:13:03 +0300

    modals style

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,158 @@
+class ServiceCart {
+  constructor() {
+    this.cartContainer = document.querySelector('#modal-teaching');
+    this.cart = {};
+    this.addEventListeners();
+    this.productService = new Services();
+  }
+  addEventListeners() {
+    this.cartContainer
+      .querySelector('.order')
+      .addEventListener('click', ev => this.order(ev));
+  }
+  async renderCart() {
+    let cartDomSting = ``;
+    for (const id in this.cart) {
+      const service = await this.productService.getServicesById(id);
+      cartDomSting += `
+                <div class="row align-items-center" data-id="${id}">
+                  <div class="col-5">
+                    <img class="img-fluid" src="${service.image}" alt="${service.title}">
+                  </div>
+                <div class="col-6 text-center">
+                    <div class="mt-3">
+                        <h3 id="title">${service.title}</h3>
+                        <h4 class="text-center mt-3">Вартість: <span id="price">${service.price}</span> грн</h4>
+                    </div>
+                  </div>
+                <div class="col-12 mt-3">
+                  <p>${service.description}</p>
+                </div>
+              </div>`;
+      }
+    this.cartContainer.querySelector(
+      '.cart-product-list-container'
+    ).innerHTML = cartDomSting;
+  }
+  addProduct(id) {
+    this.cart = {};
+    this.cart[id] = (this.cart[id] || 0);
+  }
+  order(ev) {
+    const form = this.cartContainer.querySelector('.form-contacts');
+    if (form.checkValidity()) {
+      ev.preventDefault();
+      fetch('order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            clientName: document.querySelector('#client-name').value,
+            clientРhone: document.querySelector('#client-phone').value,
+            service: document.querySelector('#title').innerHTML,
+            price: document.querySelector('#price').innerHTML,
+            cart: this.cart,
+          })
+        })
+        .then(response => {
+          if (response.status === 200) {
+            return response.text();
+          } else {
+            throw new Error('Cannot send form');
+          }
+        })
+        .then(responseText => {
+          form.reset();
+          this.cart = {};
+          window.showAlert('Дякую! ' + responseText);
+          this.cartContainer.querySelector('.btn-close').click();
+        })
+        .catch(error => showAlert('Помилка: ' + error, false));
+    } else {
+      window.showAlert('Буль ласка зоповніть коректно форму', false);
+    }
+  }
+}
+
+class TeachingCart {
+  constructor() {
+    this.cartContainer = document.querySelector('#modal-teaching');
+    this.cart = {};
+    this.addEventListeners();
+    this.productService = new TeachService();
+  }
+  addEventListeners() {
+    this.cartContainer
+      .querySelector('.order')
+      .addEventListener('click', ev => this.order(ev));
+  }
+  async renderCart() {
+    let cartDomSting = ``;
+    for (const id in this.cart) {
+      const plan = await this.productService.getTeachsById(id);
+      cartDomSting += `
+                <div class="row align-items-center" data-id="${id}">
+                  <div class="col-5">
+                    <img class="img-fluid" src="${plan.image}" alt="${plan.title}">
+                  </div>
+                <div class="col-6 text-center p-0">
+                    <div class="mt-3">
+                        <h3 id="titlePlan">${plan.title}</h3>
+                        <h4 class="text-center mt-3">Вартість:</h4>
+                        <h4><span id="price">${plan.price}</span> грн</h4>
+                    </div>
+                  </div>
+                <div class="col-12 mt-3">
+                  <p>${plan.description}</p>
+                </div>
+              </div>`;
+      }
+    this.cartContainer.querySelector(
+      '.cart-product-list-container'
+    ).innerHTML = cartDomSting;
+  }
+  addProduct(id) {
+    this.cart = {};
+    this.cart[id] = (this.cart[id] || 0);
+  }
+  order(ev) {
+    const form = this.cartContainer.querySelector('.form-contacts');
+    if (form.checkValidity()) {
+      ev.preventDefault();
+      fetch('order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            clientName: document.querySelector('#client-name').value,
+            clientРhone: document.querySelector('#client-phone').value,
+            plan: document.querySelector('#titlePlan').innerHTML,
+            price: document.querySelector('#price').innerHTML,
+            cart: this.cart,
+          })
+        })
+        .then(response => {
+          if (response.status === 200) {
+            return response.text();
+          } else {
+            throw new Error('Cannot send form');
+          }
+        })
+        .then(responseText => {
+          form.reset();
+          this.cart = {};
+          window.showAlert('Дякую! ' + responseText);
+          this.cartContainer.querySelector('.btn-close').click();
+        })
+        .catch(error => showAlert('Помилка: ' + error, false));
+    } else {
+      window.showAlert('Буль ласка зоповніть коректно форму', false);
+    }
+  }
+}
+
 class Cart {
   constructor() {
     this.cartContainer = document.querySelector('#modal-products');
@@ -15,9 +170,9 @@ class Cart {
     for (const id in this.cart) {
       const product = await this.productService.getProductById(id);
       cartDomSting += `
-                <div class="row" data-id="${id}">
+                <div class="row align-items-center" data-id="${id}">
                 <div class="col-5">
-                    <img class="card-img-top card-img-description" src="img/products/${product.image[0]}">
+                    <img class="img-fluid" src="img/products/${product.image[0]}">
                 </div>
                 <div class="col-6">
                 <div class="mt-3">
@@ -69,15 +224,12 @@ class Cart {
         .then(responseText => {
           form.reset();
           this.cart = {};
-          // this.saveCart();
-          // this.updateBadge();
-          //this.renderCart();
-          window.showAlert('Thank you! ' + responseText);
+          window.showAlert('Дякую! ' + responseText);
           this.cartContainer.querySelector('.btn-close').click();
         })
-        .catch(error => showAlert('Ошыбка: ' + error, false));
+        .catch(error => showAlert('Помилка: ' + error, false));
     } else {
-      window.showAlert('Пожалуйста заполните правильно форму', false);
+      window.showAlert('Буль ласка зоповніть коректно форму', false);
     }
   }
 }
--- js/product-list.js
+++ js/product-list.js
@@ -1,8 +1,133 @@
+class SecviceList {
+  constructor(cart) {
+      this.cart = cart;
+      this.containerSecvicesHolistic = document.querySelector('.secvices-holistic');
+      this.conteinerServicesTerraude = document.querySelector('.secvices-terraude');
+      this.containerServicePrograms = document.querySelector('.service-programs');
+      this.productService = new Services();
+      this.productService
+        .getService()
+        .then(() => this.renderServices())
+        .then(() => this.addEventListeners());
+    }
+    async renderServices() {
+      let productListDomString = '';
+      const secvices = await this.productService.getService();
+      secvices.forEach((service) => {
+          if(service.position === 'right') {
+            productListDomString += `
+            <div class="col-12 col-lg-6 mb-4 ${service.category}">
+                    <div class="card">
+                    <div class="row no-gutters">
+                        <div class="col-md-7 order-1 order-md-0"">
+                            <div class="card-body">
+                                <h3 class="card-title">${service.title}</h3>
+                                <p class="card-text">${service.description}</p>
+                                <button class="btn-main btn-accent btn-service buy-service" id="sreviceSend" data-id="${service.id}" data-toggle="modal" data-target="#modal-teaching">Замовити</button>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                          <img  class="card-img-top h-100" src="${service.image}" alt="${service.title}">
+                      </div>
+                    </div>
+                </div>
+                </div>
+            `
+          } else {
+            productListDomString += `
+              <div class="col-12 col-lg-6 mb-4 ${service.category}">
+              <div class="card">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <img class="card-img-top h-100" src="${service.image}"  alt="${service.title}">
+                    </div>
+                    <div class="col-md-7">
+                        <div class="card-body">
+                            <h3 class="card-title">${service.title}</h3>
+                            <p class="card-text">${service.description}</p>
+                            <button class="btn-main btn-accent btn-service buy-service" id="sreviceSend" data-id="${service.id}" data-toggle="modal" data-target="#modal-teaching">Замовити</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+              `;
+          }
+        });
+      this.containerSecvicesHolistic.innerHTML = productListDomString;
+      this.conteinerServicesTerraude.innerHTML = productListDomString;
+      this.containerServicePrograms.innerHTML = productListDomString;
+    }
+
+    addEventListeners() {
+      document
+        .querySelectorAll('.buy-service')
+        .forEach(button =>
+          button.addEventListener('click', event =>
+            this.handleProductBuyClick(event)
+          )
+        );
+    }
+    handleProductBuyClick(event) {
+      const button = event.target;
+      const id = button.dataset.id;
+      this.cart.addProduct(id);
+      this.cart.renderCart();
+    }
+}
+
+class TeachingList {
+    constructor(cart) {
+        this.cart = cart;
+        this.container = document.querySelector('.teach-container');
+        this.productService = new TeachService();
+        this.productService
+          .getTeachs()
+          .then(() => this.renderTeachs())
+          .then(() => this.addEventListeners());
+      }
+      async renderTeachs() {
+        let productListDomString = '';
+        const teachs = await this.productService.getTeachs();
+        teachs.forEach((teach) => {
+          productListDomString += `
+                        <div class="col-lg-4 mb-4 mb-lg-0 table-content wow fadeInDown animated animated" data-wow-delay="0.2s" style="visibility: visible; -webkit-animation-delay: 0.2s; -moz-animation-delay: 0.2s; animation-delay: 0.2s;">
+                             <div class="table-item ${teach.category}">
+                                <h3>${teach.title}</h3>
+                                <ul>
+                                    <li>Full Body Massage</li>
+                                    <li>Deep Tissue Massage</li>
+                                    <li>Hot Stone Massage</li>
+                                    <li>Body Polish (1 hr)</li>
+                                </ul>
+                                <button class="btn-main btn-accent buy-teach order" data-toggle="modal" data-target="#modal-teaching" data-id="${teach.id}">Записатись</button>
+                            </div>
+                        </div>
+                    `;
+        });
+        this.container.innerHTML = productListDomString;
+      }
+
+      addEventListeners() {
+        document
+          .querySelectorAll('.buy-teach')
+          .forEach(button =>
+            button.addEventListener('click', event =>
+              this.handleProductBuyClick(event)
+            )
+          );
+      }
+      handleProductBuyClick(event) {
+        const button = event.target;
+        const id = button.dataset.id;
+        this.cart.addProduct(id);
+        this.cart.renderCart();
+      }
+}
+
 class ProductList {
   constructor(cart) {
     this.cart = cart;
-    // this.modal = document.querySelector('#productInfoModal');
-    // this.modalImages = document.querySelector('.works-slideshow');
     this.container = document.querySelector('.products-container-one');
     this.containerTwo = document.querySelector('.products-container-two');
 
@@ -32,7 +157,7 @@ class ProductList {
         messageSaleOrNew += `<div class="banner-message new">new</div>`;
       }
 
-      productListDomString += `<div class="col-6 col-md-4 col-lg-3 mb-3 prod ${product.category}">
+      productListDomString += `<div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3 prod ${product.category}">
                   <div class="card product">
                     ${messageSaleOrNew}
                     <img class="card-img-top card-info-cursor" src="img/products/${product.image[0]}"
@@ -57,13 +182,6 @@ class ProductList {
 
   addEventListeners() {
     document
-      .querySelectorAll('.product img')
-      .forEach(button =>
-        button.addEventListener('click', event =>
-          this.handleProductInfoClick(event)
-        )
-      );
-    document
       .querySelectorAll(
         '#productInfoModal a.buy, .card.product button.buy'
       )
@@ -80,31 +198,6 @@ class ProductList {
           this.handleTabProduct(event)
         )
       );
-  }
-  async handleProductInfoClick(event) {
-    const button = event.target; // Button that triggered the modal
-    const id = button.dataset.id; // Extract info from data-* attributes
-    const product = await this.productService.getProductById(id);
-
-    // let productImageString = '';
-
-    // product.image.forEach((img, index) => {
-
-    //   productImageString += `
-    //   <div class="detail-screen-carousel-item">
-    //   <div class="item-preview">
-    //       <img class="card-img-top gallery-image" src="img/products/${img}" alt="${product.title}">
-    //   </div>
-    //     </div>
-    //   `
-    // });
-
-    // this.modalImages.innerHTML = productImageString;
-    // this.modal.querySelector('.modal-body .card-title').innerText = product.title;
-    // this.modal.querySelector('.modal-body .card-text').innerText = product.description;
-    // const btnBuy = this.modal.querySelector('a.buy');
-    // btnBuy.innerText = `Оформить заказ`
-    // btnBuy.dataset.id = id;
   }
   handleProductBuyClick(event) {
     const button = event.target;
